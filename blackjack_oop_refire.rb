3362a5d924b7c47b67cb6cfa4e92c893d11f3e09
Cole Busby <colebusby@gmail.com>
2013-07-13 14:06:44 -0600

    completed oop blackjack, with multiplayer

--- blackjack_oop_refire.rb
+++ blackjack_oop_refire.rb
@@ -75,6 +75,10 @@ module PlayerDealerHand
     total.select { |x| x < 22 } == []
   end
 
+  def blackjack?
+    total.include?(21)
+  end
+
   def show_hand
     puts 
     puts "----#{name}'s current cards:----".center(44)
@@ -82,6 +86,14 @@ module PlayerDealerHand
     puts
     puts "----#{name}'s card value is: #{total}----".center(44)
     puts
+  end
+
+  def final_total
+    value = [0]
+    total.each do |number|
+      value.replace([number]) if number > value[0] && number < 22
+    end
+    value[0]
   end
 end
 
@@ -112,84 +124,166 @@ class Dealer
     puts hand[0].to_s.center(44)
     puts
   end
+
+  def dealer_17?
+    total.select { |x| x > 16 } != []
+  end
 end
 
 class Blackjack
-  attr_accessor :player, :dealer, :deck
+  attr_accessor :players, :dealer, :deck
 
   def initialize
-    @player = Player.new("Player1")
+    @players = []
     @dealer = Dealer.new
     @deck = Deck.new
   end
 
-  def player_name
-    puts "Please enter your name"
-    player.name = gets.chomp
+  def player_names
+    while true
+      puts "Please type in a name followed by the 'Enter' key for each player."
+      puts "When you are finished, hit 'Enter' one more time."
+      answer = gets.chomp
+      break if answer == ""
+      players << answer
+    end
+    players.map! { |name| name = Player.new(name) }
   end
 
   def deal_cards
     2.times do
-      player.hand << deck.deal_card
+      players.each { |player| player.hand << deck.deal_card }
       dealer.hand << deck.deal_card
     end
-    player.show_hand
-    puts
-    dealer.show_flop
-    puts "-----Enter to Continue-----".center(44)
-    gets
-    puts
-    puts
   end
 
   def player_turn
-    while !player.busted?
-      puts
-      puts "Make a choice from the following options"
-      puts "----------------------------------------"
-      puts "1........Hit"
-      puts "2........Review Hand"
-      puts "3........Look at dealer's card"
-      puts "4........Stay"
-      puts
-      choice = gets.chomp
-      case choice
-      when "1"
-        player.hand << deck.deal_card
-        player.show_hand
-        puts "-----Enter to Continue-----".center(44)
-        gets
+    players.each do |player|
+      player.show_hand
+      dealer.show_flop
+      puts "-----Enter to Continue-----".center(44)
+      gets
+      while !player.busted? && !player.blackjack?
         puts
+        puts "Make a choice from the following options"
+        puts "----------------------------------------"
+        puts "1........Hit"
+        puts "2........Review Hand"
+        puts "3........Look at dealer's card"
+        puts "4........Stay"
         puts
-      when "2"
-        player.show_hand
-        puts "-----Enter to Continue-----".center(44)
-        gets
-        puts
-        puts
-      when "3"
-        dealer.show_flop
-        puts "-----Enter to Continue-----".center(44)
-        gets
-        puts
-        puts
-      when "4"
-        break
-      else
-        player_turn
+        choice = gets.chomp
+        case choice
+        when "1"
+          player.hand << deck.deal_card
+          player.show_hand
+          puts "-----Enter to Continue-----".center(44)
+          gets
+          puts
+        when "2"
+          player.show_hand
+          puts "-----Enter to Continue-----".center(44)
+          gets
+          puts
+        when "3"
+          dealer.show_flop
+          puts "-----Enter to Continue-----".center(44)
+          gets
+          puts
+        when "4"
+          puts "#{player.name} stays."
+          break
+        else
+          puts "Please choose from the available choices."
+        end
       end
     end
-    #blackjack_or_bust
-    puts "#{player.name} stays."
+  end
+
+  def dealer_turn
+    while !dealer.dealer_17? && !dealer.blackjack?
+      dealer.hand << deck.deal_card
+    end
+  end
+
+  def end_game
+    #For players that busted.
+    players.each do |player|
+      if player.busted?
+        puts "#{player.name} busted! #{player.name} loses."
+      end
+    end
+
+    #For players that hit Blackjack.
+    players.each do |player|
+      if player.blackjack?
+        puts "#{player.name} hit Blackjack! #{player.name} wins!"
+      end
+    end
+
+    #For dealer bust.
+    if dealer.busted?
+      dealer.show_hand
+      puts "The dealer busted! The house loses!"
+      puts
+      players.each do |player|
+        if !player.busted? && !player.blackjack?
+          puts "#{player.name} wins!"
+        end
+      end
+      puts "-----Enter to Continue-----".center(44)
+      gets
+      new_game
+    end
+
+    #For dealer stay.
+    dealer.show_hand
+    puts
+    puts "Dealer stays"
+    players.each do |player|
+      if !player.busted? && !player.blackjack?
+        if player.final_total > dealer.final_total
+          puts "#{player.name} wins!"
+          puts "#{player.name}'s total is: #{player.final_total}  The dealer's total is: #{dealer.final_total}"
+        end
+        if player.final_total == dealer.final_total
+          puts "#{player.name} pushed!"
+          puts "#{player.name}'s total is: #{player.final_total}  The dealer's total is: #{dealer.final_total}"
+        end
+        if player.final_total < dealer.final_total
+          puts "#{player.name} loses!"
+          puts "#{player.name}'s total is: #{player.final_total}  The dealer's total is: #{dealer.final_total}"
+        end
+      end
+    end
+    new_game
+  end
+
+  def new_game
+    puts
+    puts "Would you like to play again?"
+    puts "1......Yes"
+    puts "2......No"
+    answer = gets.chomp
+    if answer == "1"
+      deck = Deck.new
+      dealer.hand = []
+      players.each do |player|
+        player.hand = []
+      end
+      start
+    else
+      exit
+    end
   end
 
   def start
-    player_name
+    player_names if players == []
     deal_cards
     player_turn
-    #dealer_turn
-    #end_game
-    #new_game
+    dealer_turn
+    end_game
+    new_game
   end
 end
 
