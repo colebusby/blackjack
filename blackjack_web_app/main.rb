3665b95ce65e96f090d713c53945d4e27082ceff
Cole Busby <colebusby@gmail.com>
2013-07-17 16:19:34 -0600

    Working game with both play and dealer turns. No new game yet.

--- blackjack_web_app/main.rb
+++ blackjack_web_app/main.rb
@@ -35,10 +35,40 @@ helpers do
     total(dealer_or_player_hand).include?(21)
   end
 
+  def dealer_17?
+    total(session[:dealer_hand]).select { |x| x > 16 && x < 21 } != []
+  end
+
+  def final_total(dealer_or_player_hand)
+    value = [0]
+    total(dealer_or_player_hand).each do |number|
+      value.replace([number]) if number > value[0] && number < 22
+    end
+    value[0]
+  end
+
+  def determine_winner
+    if final_total(session[:player_hand]) > final_total(session[:dealer_hand])
+      #player wins
+      @success = "#{session[:player_name]} wins! #{session[:player_name]}'s total is: #{final_total(session[:player_hand])}  Dealer's total is: #{final_total(session[:dealer_hand])}"
+      #would you like to play again
+    elsif final_total(session[:player_hand]) == final_total(session[:dealer_hand])
+      #player ties
+      @error = "#{session[:player_name]} pushes. #{session[:player_name]}'s total is: #{final_total(session[:player_hand])}  Dealer's total is: #{final_total(session[:dealer_hand])}"
+      #would you like to play again
+    elsif final_total(session[:player_hand]) < final_total(session[:dealer_hand])
+      #player loses
+      @error = "#{session[:player_name]} loses! #{session[:player_name]}'s total is: #{final_total(session[:player_hand])}  Dealer's total is: #{final_total(session[:dealer_hand])}"
+      #would you like to play again
+    end
+  end
+
 end
 
 before do
-  @bust_or_stay = true  
+  @bust_or_stay = true
+  @dealer_turn = false
+  @game_over = false
 end
 
 get '/' do
@@ -59,6 +89,7 @@ post '/new_player' do
 end
 
 get '/game' do
+  #Dealing cards
   suits = ["Clubs", "Diamonds", "Spades", "Hearts"]
   values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
   session[:deck] = values.product(suits).shuffle!
@@ -68,14 +99,19 @@ get '/game' do
     session[:dealer_hand] << session[:deck].pop
     session[:player_hand] << session[:deck].pop
   end
+  #session[:dealer_hand] = [["Ace", "Spades"], ["Five", "Clubs"]]
+
+  #Special conditions after initial deal
   if blackjack?(session[:player_hand]) && blackjack?(session[:dealer_hand])
     @error = "Both you and the dealer were dealt a blackjack. The hand ends in a push."
     @bust_or_stay = false
+    @game_over = true
     #push
     #would you like to play again?
   elsif blackjack?(session[:player_hand])
     @success = "You hit blackjack!"
     @bust_or_stay = false
+    @game_over = true
     #player wins
     #would you like to play again?
   elsif blackjack?(session[:dealer_hand])
@@ -90,11 +126,13 @@ post '/game/player/hit' do
   if busted?(session[:player_hand])
     @error = "Sorry, you busted. Better luck next time!"
     @bust_or_stay = false
+    @game_over = true
     #player loses
     #would you like to play again?
   elsif blackjack?(session[:player_hand])
     @success = "You hit blackjack!"
     @bust_or_stay = false
+    @game_over = true
     #player wins
     #would you like to play again      
   end
@@ -105,4 +143,37 @@ post '/game/player/stay' do
   @success = "You chose to stay."
   @bust_or_stay = false
   erb :game
+end
+
+post '/game/dealer/turn' do
+  @dealer_turn = true
+  @bust_or_stay = false
+  if blackjack?(session[:dealer_hand]) || dealer_17?
+    @game_over = true
+    determine_winner
+    #would you like to play again
+  end
+
+  erb :game
+end
+
+post '/game/dealer/hit' do
+  @bust_or_stay = false
+  @dealer_turn = true
+  session[:dealer_hand] << session[:deck].pop
+  if busted?(session[:dealer_hand])
+    @success = "The dealer busted. You win!"
+    @game_over = true
+    #Player wins
+    #would you like to play again
+  elsif blackjack?(session[:dealer_hand]) || dealer_17?
+    @game_over = true
+    determine_winner
+    #would you like to play again
+  end
+  erb :game
+end
+
+post '/game/new_round' do
+  redirect '/game'
 end
