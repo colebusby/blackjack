1e541eec250cb1b7e96fd214a74df26212704746
Cole Busby <colebusby@gmail.com>
2013-07-17 10:44:24 -0600

    web blackjack with player turn and images for cards

--- blackjack_web_app/main.rb
+++ blackjack_web_app/main.rb
@@ -6,12 +6,13 @@ require 'pry'
 set :sessions, true
 
 helpers do
-  CARD_VALUES = {ace:"ace", two:2, three:3, four:4, five:5, six:6, seven:7, eight:8, nine:9, ten:10, jack:10, queen:10, king:10}
+  CARD_VALUES = {ace:"ace", two:2, three:3, four:4, five:5, six:6, seven:7, eight:8, nine:9, ten:10, jack:"jack", queen:"queen", king:"king"}
 
   def total(cards)
     arr = cards.map { |card| CARD_VALUES[card[0].downcase.to_sym] }
     value = []
-    value << arr.select { |e| e != "ace" }.inject(0, :+)
+    value << arr.select { |e| e.is_a?(Integer) }.inject(0, :+)
+    arr.select { |e| e.is_a?(String) && e != "ace" }.size.times { value[0] += 10 }
     if arr.include?("ace")    
       arr.select { |e| e == "ace" }.count.times do
         value2 = value
@@ -28,6 +29,10 @@ helpers do
 
   def busted?(dealer_or_player_hand)
     total(dealer_or_player_hand).select { |x| x < 22 } == []
+  end
+
+  def blackjack?(dealer_or_player_hand)
+    total(dealer_or_player_hand).include?(21)
   end
 
 end
@@ -63,6 +68,19 @@ get '/game' do
     session[:dealer_hand] << session[:deck].pop
     session[:player_hand] << session[:deck].pop
   end
+  if blackjack?(session[:player_hand]) && blackjack?(session[:dealer_hand])
+    @error = "Both you and the dealer were dealt a blackjack. The hand ends in a push."
+    @bust_or_stay = false
+    #push
+    #would you like to play again?
+  elsif blackjack?(session[:player_hand])
+    @success = "You hit blackjack!"
+    @bust_or_stay = false
+    #player wins
+    #would you like to play again?
+  elsif blackjack?(session[:dealer_hand])
+    @error = "Oh no, the dealer was dealt a blackjack! The best you can hope for is a push this hand."
+  end
 
   erb :game
 end
@@ -72,6 +90,13 @@ post '/game/player/hit' do
   if busted?(session[:player_hand])
     @error = "Sorry, you busted. Better luck next time!"
     @bust_or_stay = false
+    #player loses
+    #would you like to play again?
+  elsif blackjack?(session[:player_hand])
+    @success = "You hit blackjack!"
+    @bust_or_stay = false
+    #player wins
+    #would you like to play again      
   end
   erb :game
 end
